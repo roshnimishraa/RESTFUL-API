14166fa7430952c58d336347e2c8be0397a2dc30
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2023-03-05 21:34:47 +0530

    Add files via upload

---
+++ Restful API/src/db/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/students-api", {
+    // useCreateIndex: true,
+    // useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(()=> {
+    console.log("Connection successful");
+}).catch((err)=> {
+    console.log("Connection failed!");
+})
---
+++ Restful API/src/model/students.js
@@ -0,0 +1,36 @@
+const mongoose=require("mongoose");
+const validator = require("validator");
+
+const studentSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minlength:2
+    },
+    email:{
+type:String,
+unique:[true,"Email id already exists"],
+required:true,
+validate(value){
+    if(!validator.isEmail(value)){
+    throw new Error("Invalid email");
+    }
+}
+    },
+    phone:{
+        type:Number,
+        minlength:10,
+        maxlength:10,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    }
+})
+
+// model
+const student=new mongoose.model('student',studentSchema);
+
+// module export
+module.exports = student;
